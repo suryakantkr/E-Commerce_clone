9d1324146db73f1559bf49d618d17d16c978badd
suryakantkr <suryakantkr636@gmail.com>
2025-10-20 01:12:17 +0530

    add media for mobile

--- src/componets/Navbar/Nav.js
+++ src/componets/Navbar/Nav.js
@@ -9,28 +9,29 @@ import nav_dropdown from '../../Assets/nav_dropdown.png';
 
 function Nav() {
     const [menu, setmenu] = useState("shop");
-    const {getTotalCartItems}=useContext(ShopContext);
-    const menuRef=useRef();
+    const { getTotalCartItems } = useContext(ShopContext);
+    const menuRef = useRef();
 
-    const dropdown_toggle=(e)=>{
-        menuRef.current.classList.toggle("nav-menu-visible");
-        e.target.classList.toggle('open');
-    }
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.currentTarget.classList.toggle('open');
+    };
+    
     return (
         <div className="navbar">
             <div className='navbar-logo'>
                 <img src={Logo} alt='logo-img' />
             </div>
             <div className="search-bar">
-               <CiSearch className="search-icon" />
-              <Link to='/search'><input type="text" placeholder="Search any products" /></Link> 
+                <CiSearch className="search-icon" />
+                <Link to='/search'><input type="text" placeholder="Search any products" /></Link>
             </div>
-            <img onClick={dropdown_toggle} src={nav_dropdown} alt=""/>
+            <img onClick={dropdown_toggle} src={nav_dropdown} alt="menu" className="nav-dropdown" />
             <ul ref={menuRef} className="navbar-menu">
-                <li onClick={() => { setmenu("shop") }}><Link style={{textDecoration:"none"}} to='/'>Shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
-                <li onClick={() => { setmenu("mens") }}><Link style={{textDecoration:"none"}} to='/mens'>Mens</Link> {menu === "mens" ? <hr /> : <></>}</li>
-                <li onClick={() => { setmenu("women") }}><Link style={{textDecoration:"none"}} to='/womens'>Womens</Link>{menu === "women" ? <hr /> : <></>}</li>
-                <li onClick={() => { setmenu("kids") }}><Link style={{textDecoration:"none"}} to='/kids'>Kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
+                <li onClick={() => { setmenu("shop") }}><Link style={{ textDecoration: "none" }} to='/'>Shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
+                <li onClick={() => { setmenu("mens") }}><Link style={{ textDecoration: "none" }} to='/mens'>Mens</Link> {menu === "mens" ? <hr /> : <></>}</li>
+                <li onClick={() => { setmenu("women") }}><Link style={{ textDecoration: "none" }} to='/womens'>Womens</Link>{menu === "women" ? <hr /> : <></>}</li>
+                <li onClick={() => { setmenu("kids") }}><Link style={{ textDecoration: "none" }} to='/kids'>Kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
             </ul>
             <div className="navbar-login-cart">
                 <Link to='/login'><button>Login</button></Link>
