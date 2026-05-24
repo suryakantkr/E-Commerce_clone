d22372307ebdd4505c482158b083f02bd6df6469
suryakantkr <suryakantkr636@gmail.com>
2026-05-24 11:30:04 +0530

    remove alt in item.jsx

--- frontend/src/componets/Item/Item.jsx
+++ frontend/src/componets/Item/Item.jsx
@@ -5,7 +5,7 @@ const Item = (props) => {
     return (
         <div className='item'>
             <Link to={`/product/${props.id}`}>
-                <img onClick={window.scrollTo(0,0)} src={props.image} alt="image item" />
+                <img onClick={window.scrollTo(0,0)} src={props.image} />
             </Link>
             <p>{props.name}</p>
             <div className='item-prices'>
