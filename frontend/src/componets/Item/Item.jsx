ba35751fe758383f8ebc1d918aa21eaeaead467c
suryakantkr <suryakantkr636@gmail.com>
2026-05-24 11:37:57 +0530

    add props and product

--- frontend/src/componets/Item/Item.jsx
+++ frontend/src/componets/Item/Item.jsx
@@ -5,7 +5,7 @@ const Item = (props) => {
     return (
         <div className='item'>
             <Link to={`/product/${props.id}`}>
-                <img onClick={window.scrollTo(0,0)} src={props.image} />
+                <img onClick={window.scrollTo(0,0)} src={props.image} alt={props.name || "product"} />
             </Link>
             <p>{props.name}</p>
             <div className='item-prices'>
